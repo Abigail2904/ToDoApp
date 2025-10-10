37e4002a9c39ed83f9176376eb90215a1fcd76e2
Madeleine <maddiedeabreu04@gmail.com>
2025-10-10 06:59:19 -0400

    Agrega archivo .gitignore para ignorar node_modules, .env

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+
+
+(async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI_TEST);  
+        console.log('Conectado a MongoDB');     
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }    
+})();
+
+
+//Rutas frontend
+app.use('/', express.static(path.resolve('views','home')));
+app.use('/signup', express.static(path.resolve('views','signup')));
+app.use('/login', express.static(path.resolve('views','login')));
+app.use('/components', express.static(path.resolve('views','components')));
+
+
+
+
+module.exports = app;
